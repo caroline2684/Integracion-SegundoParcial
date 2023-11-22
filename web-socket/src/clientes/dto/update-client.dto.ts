3bc1a35719009c08dfe3fa2119155453dd235caf
caroline2684 <carolineliseth18@gmail.com>
2023-11-21 22:56:50 -0500

    a

--- web-socket/src/clientes/dto/update-client.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateClient } from './create-client.dto'
-import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator';
-
-export class UpdateClient extends PartialType(CreateClient) {
-    @IsBoolean()
-    @IsOptional()
-    estado?: boolean;
-}
